a257a0e9bf66cee902f04870e1cb1f8c4045e4aa
JoeCT_Chang <JoeCT.Chang@moxa.com>
2025-03-24 15:09:48 +0800

    feat: add fontawesome

---
+++ src/components/icon.ts
@@ -0,0 +1,5 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCoffee, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faCoffee, faUser, faGoogle);
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold text-gray-900 mb-4">404</h1>
+        <p className="text-xl text-gray-600 mb-8">找不到頁面</p>
+        <Link
+          to="/"
+          className="inline-block px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
+        >
+          返回首頁
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
