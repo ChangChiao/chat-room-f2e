6932df6cdc0a79273879c10c90b83dff228503c4
JoeCT_Chang <JoeCT.Chang@moxa.com>
2025-04-01 08:48:46 +0800

    feat: add env varible for strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root")!);
+const isDev = import.meta.env.DEV;
+
+if (isDev) {
+  root.render(<App />);
+} else {
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
